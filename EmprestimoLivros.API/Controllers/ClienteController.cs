51baa69d7405d8fa5bec5ce23fbf3070d1193f9a
Leocaldio Carlos <leocaldiocarloss@gmail.com>
2025-07-17 01:29:04 +0100

    First Commit

---
+++ EmprestimoLivros.API/Controllers/ClienteController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using EmprestimoLivros.API.DTOs;
+using EmprestimoLivros.API.Interfaces;
+using EmprestimoLivros.API.Models;
+using EmprestimoLivros.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {
+        private readonly IClienteRepository _clienteRepository;
+        private readonly IMapper _mapper;
+        public ClienteController(IClienteRepository clienteRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _clienteRepository = clienteRepository;
+        }   
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> SelecionarTodosClientes()
+        {
+            return Ok(await _clienteRepository.SelecionarTodosAsync());
+
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> CadastrarCliente(Cliente cliente)
+        {
+
+            _clienteRepository.Incluir(cliente);
+            if (await _clienteRepository.SaveAllAsync()) return Ok("Cliente cadastrado com sucsso!");
+            else return BadRequest("Ocorreu algum erro ao salvar CLiente!");
+        }
+        [HttpPut]
+        public async Task<ActionResult> AlterarCLiente(Cliente cliente)
+        {
+            _clienteRepository.Alterar(cliente);
+            if (await _clienteRepository.SaveAllAsync()) return Ok("Cliente alterado com sucesso!");
+            else return BadRequest("Ocorreu algum erro ao alterar Cliente!");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> ExcluirCLiente(int id)
+        {
+           var cliente = await _clienteRepository.SelecionarByIdAsync(id);
+
+            if (cliente == null) return NotFound("Cliente não encontrado!");
+
+            _clienteRepository.Excluir(cliente);
+
+            if (await _clienteRepository.SaveAllAsync())
+            {
+                return Ok("Cliente eclido com sucesso!");
+            }
+            return BadRequest("Falha ao excluir cliente");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> SelecionarPorId(int id)
+        {
+            var cliente = await _clienteRepository.SelecionarByIdAsync(id);
+            if (cliente == null)
+            {
+                return NotFound("Cliente não encontrado!");
+            }
+            
+            var clienteDTO = _mapper.Map<ClienteDTO>(cliente);
+            return Ok(clienteDTO);
+            
+        }
+    }
+}
---
+++ EmprestimoLivros.API/Interfaces/ILivroRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmprestimoLivros.API.Models;
+
+namespace EmprestimoLivros.API.Interfaces
+{
+    public interface ILivroRepository
+    {
+        void Incluir(Livro cliente);
+        void Alterar(Livro cliente);
+        void Excluir(int id);
+        Task<Livro> SelecionarByIdAsync(int id);
+        Task<IEnumerable<Livro>> SelecionarTodosAsync();
+    }
+}
